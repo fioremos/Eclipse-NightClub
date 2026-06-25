ed6845378882b93fedb80e9059d20c9934e033fc
Fiorella <fioremos@yahoo.com.ar>
2026-06-24 23:29:45 -0300

    Refactor settings

--- mi_sitio/settings.py
+++ mi_sitio/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleWare'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mi_sitio.urls'
